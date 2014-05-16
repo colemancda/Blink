7b0877488332d3338c49508d95c4feb7bfd3763d
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-16 13:45:35 -0400

    first commit

---
+++ Blink/Blink.ino
@@ -0,0 +1,71 @@
+// 
+// Blink 
+//
+// Blinks
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+// 
+// Author	 	Alsey Coleman Miller
+// 				ColemanCDA
+//
+// Date			5/16/14 1:17 PM
+// Version		1.0
+// 
+// Copyright	© Alsey Coleman Miller, 2014
+// License		CC
+//
+// See			ReadMe.txt for references
+//
+
+// Core library for code-sense
+#if defined(WIRING) // Wiring specific
+#include "Wiring.h"
+#elif defined(MAPLE_IDE) // Maple specific
+#include "WProgram.h"   
+#elif defined(MICRODUINO) // Microduino specific
+#include "Arduino.h"
+#elif defined(MPIDE) // chipKIT specific
+#include "WProgram.h"
+#elif defined(DIGISPARK) // Digispark specific
+#include "Arduino.h"
+#elif defined(ENERGIA) // LaunchPad MSP430, Stellaris and Tiva, Experimeter Board FR5739 specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.0 and 1.5 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif
+
+// Include application, user and local libraries
+
+
+// Define variables and constants
+//
+// Brief	Name of the LED
+// Details	Each board has a LED but connected to a different pin
+//
+const uint8_t myLED = 12;
+
+
+//
+// Brief	Setup
+// Details	Define the pin the LED is connected to
+//
+// Add setup code 
+void setup() {
+
+    pinMode(myLED, OUTPUT);
+}
+
+//
+// Brief	Loop
+// Details	Blink the LED
+//
+// Add loop code 
+void loop() {
+    digitalWrite(myLED, HIGH);
+    delay(2000);
+    digitalWrite(myLED, LOW);
+    delay(100);
+}
---
+++ Blink/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  Blink
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project Blink
+  Created by Alsey Coleman Miller on 5/16/14
+  Copyright © 2014 ColemanCDA
+  License <#license#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2014
+  All rights reserved
+  http://embedXcode.weebly.com
+
